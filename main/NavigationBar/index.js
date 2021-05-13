763ec307473bb36c06fd80881f31c1ea1b0765d6
SernSernSern <sg.sern@gmail.com>
2021-05-13 14:06:08 +0300

    init

---
+++ main/NavigationBar/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+import './index.styl'
+
+const NavigationBar = prop => {
+   return pug`
+      View.nav
+         Button(
+            color="#dedede"
+            title="Introduction"
+         )
+         Button(
+            color="#dedede"
+            title="First Section"
+         )
+         Button(
+            color="#dedede"
+            title="Second Section"
+         )
+         Button(
+            color="#dedede"
+            title="Get Started"
+         )
+   `
+}
+
+export default NavigationBar;
